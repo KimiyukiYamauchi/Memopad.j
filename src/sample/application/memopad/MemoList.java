73282d0f67e4ec5c769edb1ccd0e99655a647379
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2012-10-10 09:29:21 +0900

    10/10 9:28 10/9授業作成分

--- src/sample/application/memopad/MemoList.java
+++ src/sample/application/memopad/MemoList.java
@@ -1,12 +1,20 @@
 package sample.application.memopad;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -38,6 +46,8 @@ public class MemoList extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.memolist);
 		showMemos(getMemos());
+		ListView lv = (ListView) this.findViewById(android.R.id.list);
+		registerForContextMenu(lv);
 	}
 
 	private void showMemos(Cursor cursor) {
@@ -61,4 +71,59 @@ public class MemoList extends ListActivity {
 		return cursor;
 	}
 
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater mi = getMenuInflater();
+		mi.inflate(R.menu.contextmenu, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+		Cursor cursor = getMemos();
+		startManagingCursor(cursor);
+		cursor.moveToPosition(info.position);
+		final int columnid = cursor.getInt(2);
+
+		AlertDialog.Builder ab = new AlertDialog.Builder(this);
+		ab.setTitle(R.string.memodb_delete);
+		ab.setMessage(R.string.memodb_confirm_delete);
+		ab.setPositiveButton(R.string.button_ok,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						SQLiteDatabase db = memos.getWritableDatabase();
+						db.delete("memoDB", "_id=" + columnid, null);
+						db.close();
+						showMemos(getMemos());
+					}
+				});
+		ab.setNegativeButton(R.string.button_cancel,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+					}
+				});
+
+		ab.show();
+
+		return super.onContextItemSelected(item);
+	}
+
+	public void searchMemo(View v) {
+		Log.d(getLocalClassName(), "searchMemo");
+		EditText et = (EditText) findViewById(R.id.editText1);
+		String q = et.getText().toString();
+		SQLiteDatabase db = memos.getReadableDatabase();
+		Cursor cursor = db.query("memoDB", cols, "memo like '%" + q + "%'",
+				null, null, null, null);
+		startManagingCursor(cursor);
+		showMemos(cursor);
+
+	}
 }
